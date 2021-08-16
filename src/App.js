b8a3808de533ce925df1f72fd3671b244e578147
unknown <tingzhang0997@gmail.com>
2021-08-16 22:10:05 +1000

    adding shoppage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import {Route, Switch} from 'react-router-dom'
+import ShopPage from './pages/shoppage/shoppage.component'
+
+
 
 function App() {
   return (
     <div >
-    <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
